5b8510ae68fc0704baed8372a02a187e2a8457b3
Apoorv <apoorvagni@hotmail.com>
2018-09-09 18:34:46 +0530

    need to do Q3

--- 2-LanguageModelling/helper.py
+++ 2-LanguageModelling/helper.py
@@ -4,8 +4,9 @@ nltk.download('punkt')
 
 # some perticulat functions
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
-# get tokenized setences present in "filename"
+# get tokenized sentences present in "filename"
 def getSents(filename):
     # reading the file and using word_tokenize
     f = open(filename, 'r', encoding="utf8")
@@ -21,4 +22,58 @@ def getSents(filename):
     """
     line = re.sub(r"\n(\n*)", "\1", raw)
     return sent_tokenize(raw)
+
+"""
+brief:
+Run a word tokenizer on the sentences generated and 
+remove any nonaplabetical words. Furter lower the
+cases in each word.
+
+params:
+takes in a list of list of strings.
+"""
+def lowerPunct(sentences):
+    r =[]
+    for sentence in sentences:
+        list_words = word_tokenize(sentence)
+        r.append([word.lower() for word in \
+                  list_words if word.isalpha()])
+    return r
+
+"""
+Add start and stop symbols to a given set of
+sentences.
+"""
+def addStartStop(sentences):
+    r =[]
+    for sentence in sentences:
+        l = ['<s>']
+        l.extend(sentence)
+        l.append('</s>')
+        r.append(l)
+    return r
+
+'''
+gives out a dictionary that has a structure
+similar to a trie.
+'''
+def nGramCount(sentences, count = 3):
+    dic = {}
+    for i in range (1, count + 1):
+        for sentence in sentences:
+            state = []
+            for word in sentence:
+                state.append(word)
+                if len(state) > i:
+                    state.pop(0)
+                if len(state) == i:
+                    # add ngram to the dictionary.
+
+def increaseCountDictionary(dic, ngram):
+    try:
+        for word in ngram:
+            if ngram[0] == word:
+                dicr =
+            dic = dic[word]
+
 
